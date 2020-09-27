33ad8583aab7f5ce9bc26717add228733d23244e
sekfung <sekfung.lau@gmail.com>
2020-09-27 19:14:08 +0800

    add aliyun auth

--- test/emqx_aliyun_auth_tests.erl
+++ test/emqx_aliyun_auth_tests.erl
@@ -6,17 +6,17 @@
 %%% @end
 %%% Created : 25. Sep 2020 15:52
 %%%-------------------------------------------------------------------
--module(emqx_aliyun_sign_tests).
+-module(emqx_aliyun_auth_tests).
 -author("sekfung").
 
 
 -include_lib("eunit/include/eunit.hrl").
--import(emqx_aliyun_sign, [sign/1]).
+-import(emqx_aliyun_auth, [gen_password/5]).
 
 
 
 sign_test() ->
-%%  SignInfo = emqx_aliyun_sign:SignInfo#{}
-%%  emqx_aliyun_sign:sign(SignInfo),
-  ?assertEqual(4, 4).
+  Result = emqx_aliyun_auth:gen_password("11111", "test1", "aa", "2524608000000", "bb"),
+  erlang:display(Result),
+  ?assertEqual("6B1994895A537C8DE9645B1CBE8DC0DFA4A7C4B", Result).
 
